a8718d0f7808bbaf7d87c65df7aa62dc1f291ef9
Igor Bubelov <igor@bubelov.com>
2019-05-04 17:14:22 +0700

    Refactor exchange rates module

--- app/src/main/java/com/bubelov/coins/rates/ExchangeRatesFragment.kt
+++ app/src/main/java/com/bubelov/coins/rates/ExchangeRatesFragment.kt
@@ -1,33 +1,5 @@
-/*
- * This is free and unencumbered software released into the public domain.
- *
- * Anyone is free to copy, modify, publish, use, compile, sell, or
- * distribute this software, either in source code form or as a compiled
- * binary, for any purpose, commercial or non-commercial, and by any
- * means.
- *
- * In jurisdictions that recognize copyright laws, the author or authors
- * of this software dedicate any and all copyright interest in the
- * software to the public domain. We make this dedication for the benefit
- * of the public at large and to the detriment of our heirs and
- * successors. We intend this dedication to be an overt act of
- * relinquishment in perpetuity of all present and future rights to this
- * software under copyright law.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- *
- * For more information, please refer to <https://unlicense.org>
- */
-
 package com.bubelov.coins.rates
 
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
@@ -41,9 +13,17 @@ import com.bubelov.coins.model.CurrencyPair
 import com.bubelov.coins.util.viewModelProvider
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.fragment_exchange_rates.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ExchangeRatesFragment : DaggerFragment() {
+    private val rootJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + rootJob)
+
     @Inject lateinit var modelFactory: ViewModelProvider.Factory
 
     private val model by lazy {
@@ -86,14 +66,21 @@ class ExchangeRatesFragment : DaggerFragment() {
 
         ratesView.layoutManager = LinearLayoutManager(requireContext())
 
-        model.currencyPair.observe(this, Observer { pair ->
-            toolbar.menu.findItem(R.id.currency).title = pair.toString()
-        })
+        uiScope.launch {
+            model.selectedPair.collect { pair ->
+                toolbar.menu.findItem(R.id.currency).title = pair.toString()
+            }
 
-        model.ratesRows.observe(this, Observer { rows ->
-            ratesView.adapter = ExchangeRatesAdapter(rows ?: emptyList())
-        })
+            model.rows.collect { rows ->
+                ratesView.adapter = ExchangeRatesAdapter(rows)
+            }
+        }
 
         model.selectCurrencyPair(CurrencyPair.BTC_USD)
+    }
+
+    override fun onDestroyView() {
+        rootJob.cancel()
+        super.onDestroyView()
     }
 }
--- app/src/main/java/com/bubelov/coins/rates/ExchangeRatesViewModel.kt
+++ app/src/main/java/com/bubelov/coins/rates/ExchangeRatesViewModel.kt
@@ -1,63 +1,38 @@
-/*
- * This is free and unencumbered software released into the public domain.
- *
- * Anyone is free to copy, modify, publish, use, compile, sell, or
- * distribute this software, either in source code form or as a compiled
- * binary, for any purpose, commercial or non-commercial, and by any
- * means.
- *
- * In jurisdictions that recognize copyright laws, the author or authors
- * of this software dedicate any and all copyright interest in the
- * software to the public domain. We make this dedication for the benefit
- * of the public at large and to the detriment of our heirs and
- * successors. We intend this dedication to be an overt act of
- * relinquishment in perpetuity of all present and future rights to this
- * software under copyright law.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- *
- * For more information, please refer to <https://unlicense.org>
- */
-
 package com.bubelov.coins.rates
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.bubelov.coins.model.CurrencyPair
 import com.bubelov.coins.repository.Result
 import com.bubelov.coins.repository.rate.ExchangeRatesRepository
 import com.bubelov.coins.repository.rate.ExchangeRatesSource
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.flow
 import java.text.NumberFormat
 import javax.inject.Inject
 
 class ExchangeRatesViewModel @Inject constructor(
     private val exchangeRatesRepository: ExchangeRatesRepository
 ) : ViewModel() {
-    private val mainJob = Job()
+    private val rootJob = Job()
     private var fetchRatesJob: Job? = null
-    private val uiScope = CoroutineScope(Dispatchers.Main + mainJob)
+    private val uiScope = CoroutineScope(Dispatchers.Main + rootJob)
 
-    private val _currencyPair = MutableLiveData<CurrencyPair>()
-    val currencyPair: LiveData<CurrencyPair> = _currencyPair
+    private lateinit var selectedPairCollector: FlowCollector<CurrencyPair>
+    val selectedPair = flow<CurrencyPair> { selectedPairCollector = this }
 
-    private val _ratesRows = MutableLiveData<List<ExchangeRateRow>>()
-    val ratesRows: LiveData<List<ExchangeRateRow>> = _ratesRows
+    private lateinit var rowsCollector: FlowCollector<List<ExchangeRateRow>>
+    val rows = flow<List<ExchangeRateRow>> { rowsCollector = this }
 
     override fun onCleared() {
         super.onCleared()
-        mainJob.cancel()
+        rootJob.cancel()
     }
 
     fun selectCurrencyPair(pair: CurrencyPair) {
-        _currencyPair.value = pair
+        uiScope.launch {
+            selectedPairCollector.emit(pair)
+        }
 
         fetchRatesJob?.cancel()
 
@@ -68,7 +43,7 @@ class ExchangeRatesViewModel @Inject constructor(
                 .map { it.toRow("Loading") }
                 .toMutableList()
 
-            _ratesRows.value = rows
+            rowsCollector.emit(rows)
 
             val results = mutableListOf<Deferred<Unit>>()
 
@@ -90,7 +65,7 @@ class ExchangeRatesViewModel @Inject constructor(
 
                     if (isActive) {
                         rows[index] = row
-                        _ratesRows.value = rows
+                        rowsCollector.emit(rows)
                     }
                 }
             }
