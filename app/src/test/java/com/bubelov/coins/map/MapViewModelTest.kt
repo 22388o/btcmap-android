e3430bc0192b2b32f6951590f493a0f64b986675
Igor Bubelov <igor@bubelov.com>
2019-05-10 12:08:07 +0700

    Fix build

--- app/src/test/java/com/bubelov/coins/map/MapViewModelTest.kt
+++ app/src/test/java/com/bubelov/coins/map/MapViewModelTest.kt
@@ -1,34 +1,9 @@
-/*
- * This is free and unencumbered software released into the public domain.
- *
- * Anyone is free to copy, modify, publish, use, compile, sell, or
- * distribute this software, either in source code form or as a compiled
- * binary, for any purpose, commercial or non-commercial, and by any
- * means.
- *
- * In jurisdictions that recognize copyright laws, the author or authors
- * of this software dedicate any and all copyright interest in the
- * software to the public domain. We make this dedication for the benefit
- * of the public at large and to the detriment of our heirs and
- * successors. We intend this dedication to be an overt act of
- * relinquishment in perpetuity of all present and future rights to this
- * software under copyright law.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- *
- * For more information, please refer to <https://unlicense.org>
- */
-
 package com.bubelov.coins.map
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.bubelov.coins.repository.area.NotificationAreaRepository
+import com.bubelov.coins.repository.currency.CurrenciesRepository
+import com.bubelov.coins.repository.currencyplace.CurrenciesPlacesRepository
 import com.bubelov.coins.repository.place.PlacesRepository
 import com.bubelov.coins.repository.placecategory.PlaceCategoriesRepository
 import com.bubelov.coins.repository.placeicon.PlaceIconsRepository
@@ -55,6 +30,9 @@ class MapViewModelTest {
     @Mock private lateinit var placeIconsRepository: PlaceIconsRepository
     @Mock private lateinit var location: LocationLiveData
     @Mock private lateinit var userRepository: UserRepository
+    @Mock private lateinit var currenciesRepository: CurrenciesRepository
+    @Mock private lateinit var currenciesPlacesRepository: CurrenciesPlacesRepository
+
     private lateinit var model: MapViewModel
 
     @Before
@@ -68,6 +46,8 @@ class MapViewModelTest {
             placeIconsRepository,
             location,
             userRepository,
+            currenciesRepository,
+            currenciesPlacesRepository,
             Dispatchers.Default
         )
     }
