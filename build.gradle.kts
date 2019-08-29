db5729fe1ebddc2ae1b86367000d266ecd9dc309
Igor Bubelov <igor@bubelov.com>
2019-08-29 20:16:32 +0700

    Update Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.31")
-        classpath("org.jetbrains.kotlin:kotlin-android-extensions:1.3.31")
+        classpath("com.android.tools.build:gradle:3.5.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.41")
+        classpath("org.jetbrains.kotlin:kotlin-android-extensions:1.3.41")
         classpath("android.arch.navigation:navigation-safe-args-gradle-plugin:1.0.0")
     }
 }
