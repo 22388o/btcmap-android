64600fd76e0977a46913ff081f6ba4ef90b659b6
Igor Bubelov <igor@bubelov.com>
2019-11-20 11:04:23 +0700

    Update build scripts

--- app/src/main/java/com/bubelov/coins/sync/DatabaseSyncScheduler.kt
+++ app/src/main/java/com/bubelov/coins/sync/DatabaseSyncScheduler.kt
@@ -1,63 +1,29 @@
-/*
- * This is free and unencumbered software released into the public domain.
- *
- * Anyone is free to copy, modify, publish, use, compile, sell, or
- * distribute this software, either in source code form or as a compiled
- * binary, for any purpose, commercial or non-commercial, and by any
- * means.
- *
- * In jurisdictions that recognize copyright laws, the author or authors
- * of this software dedicate any and all copyright interest in the
- * software to the public domain. We make this dedication for the benefit
- * of the public at large and to the detriment of our heirs and
- * successors. We intend this dedication to be an overt act of
- * relinquishment in perpetuity of all present and future rights to this
- * software under copyright law.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- *
- * For more information, please refer to <https://unlicense.org>
- */
-
 package com.bubelov.coins.sync
 
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ProcessLifecycleOwner
-import androidx.work.Constraints
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
-import timber.log.Timber
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class DatabaseSyncScheduler @Inject constructor() {
     fun schedule() {
-        val lifecycleOwner = ProcessLifecycleOwner.get()
-
-        WorkManager.getInstance().getWorkInfosByTagLiveData(TAG).observe(lifecycleOwner, Observer { info ->
-            Timber.d("Work info: $info")
-
-            if (info.isNullOrEmpty()) {
-                val constraints = Constraints.Builder()
-                    .setRequiresBatteryNotLow(true)
-                    .build()
-
-                val databaseSyncWork = PeriodicWorkRequestBuilder<DatabaseSyncWorker>(12, TimeUnit.HOURS)
-                    .setConstraints(constraints)
-                    .addTag(TAG)
-                    .build()
-
-                WorkManager.getInstance().enqueue(databaseSyncWork)
-            }
-        })
+//        val lifecycleOwner = ProcessLifecycleOwner.get()
+//
+//        WorkManager.getInstance().getWorkInfosByTagLiveData(TAG).observe(lifecycleOwner, Observer { info ->
+//            Timber.d("Work info: $info")
+//
+//            if (info.isNullOrEmpty()) {
+//                val constraints = Constraints.Builder()
+//                    .setRequiresBatteryNotLow(true)
+//                    .build()
+//
+//                val databaseSyncWork = PeriodicWorkRequestBuilder<DatabaseSyncWorker>(12, TimeUnit.HOURS)
+//                    .setConstraints(constraints)
+//                    .addTag(TAG)
+//                    .build()
+//
+//                WorkManager.getInstance().enqueue(databaseSyncWork)
+//            }
+//        })
     }
 
     companion object {
